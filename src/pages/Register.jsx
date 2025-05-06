8c9de3d313175d23d42ce4e3d62213a5c9a3904c
LinkonJT <linkontripura@gmail.com>
2025-05-06 23:11:17 +0600

    52_5_4 - [Recap] Register User & OnAuthStateChange

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,44 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router';
-
+import React, { use } from "react";
+import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
-    return (
-        <div>
-            <div className='flex justify-center min-h-screen items-center'>
+
+    const {createUser, setUser} = use (AuthContext)
+
+    const handleRegister = (e) =>{
+        e.preventDefault();
+        console.log(e.target);
+        const form = e.target;
+        const name =form.name.value;
+        const photo =form.photo.value;
+        const email =form.email.value;
+        const password =form.password.value;
+        
+        console.log({name, photo, email, password});
+        createUser(email, password).then (result=>{
+            const user=result.user;
+
+            setUser(user);
+            // console.log(user)
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert (errorMessage)
+          });
+    }
+
+
+  return (
+    <div>
+      <div className="flex justify-center min-h-screen items-center">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+        <h2 className="font-semibold text-2xl text-center">
+              Register your account
+            </h2>
+          <form onSubmit={handleRegister} className="card-body">
            
-           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
-    <div className="card-body">
+            <fieldset className="fieldset">
+              {/* Name */}
+              <label className="label">Name</label>
+              <input type="text" name="name" className="input" placeholder="Your Name" required />
 
-    <h2 className='font-semibold text-2xl text-center'>Register your account</h2>
-      <fieldset className="fieldset">
+              {/* Photo Url */}
+              <label className="label">Email</label>
+              <input type="text" name="photo" className="input" placeholder="Photo URL" required />
 
-{/* Name */}
-<label className="label">Name</label>
-<input type="text" className="input" placeholder="Your Name" />
+              {/* Email */}
+              <label className="label">Email</label>
+              <input type="email" name="email" className="input" placeholder="Email" required />
+              {/* Password */}
+              <label className="label">Password</label>
+              <input type="password" name="password" className="input" placeholder="Password" required />
+{/* Register Button */}
+              <button type="submit" className="btn btn-neutral mt-4">Register</button>
 
-{/* Photo Url */}
-<label className="label">Email</label>
-<input type="text" className="input" placeholder="Photo URL" />
-
-        {/* Email */}
-        <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
-        {/* Password */}
-        <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
-        
-
-        
-        <button className="btn btn-neutral mt-4">Register</button>
-
-        <p  className='font-semibold text-center pt-5'>Already Have An Account?{" "}<Link className='text-blue-500' to="/auth/login">Login</Link> </p>
-      </fieldset>
-    </div>
-  </div>
-      </div>
+              <p className="font-semibold text-center pt-5">
+                Already Have An Account?{" "}
+                <Link className="text-blue-500" to="/auth/login">
+                  Login
+                </Link>{" "}
+              </p>
+            </fieldset>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
